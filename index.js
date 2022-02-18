7039aa4c02510c76f10ddb5c25ca1a2ebc928da8
GuptaEesh <p1r8a5t7ibha@gmail.com>
2022-02-18 12:29:54 +0530

    reduced code fat

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ mySliders.forEach(mySlider => mySlider.addEventListener('input', () => {
 //Terms and condition dialogue box.
 let termsInfo = document.querySelector('.terms-info');
 termsInfo.addEventListener('click', () => {
-    document.querySelector('.signup-page .login-dialogue .dialogue-container').style.display = "grid";
+    document.querySelector('.dialogue-container').style.display = "grid";
 })
-document.querySelector('.tertiary.border-between-button').addEventListener('click', () => {
-    document.querySelector('.signup-page .login-dialogue .input').checked = false;
-    document.querySelector('.signup-page .login-dialogue .dialogue-container').style.display = "none";
+document.querySelector('.border-between-button').addEventListener('click', () => {
+    document.querySelector('.signup-page .input').checked = false;
+    document.querySelector('.signup-page .dialogue-container').style.display = "none";
 })
-document.querySelector('.tertiary.accept').addEventListener('click', () => {
-    document.querySelector('.signup-page .login-dialogue .input').checked = true;
-    document.querySelector('.signup-page .login-dialogue .dialogue-container').style.display = "none";
+document.querySelector('.accept').addEventListener('click', () => {
+    document.querySelector('.signup-page .input').checked = true;
+    document.querySelector('.signup-page .dialogue-container').style.display = "none";
 })
 
